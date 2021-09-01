b7c29f9ceac43ae4cff0738b126e272797ad365d
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-09-01 14:48:09 -0300

    feat: finish crud

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { AddTaskController } from "./usecases/add-user/add-task-controller";
+import { AddTaskController, LoadTasksController, UpdateTaskController, RemoveTaskController } from "./usecases/controllers";
 
 const router = Router()
 
 const addTaskController = new AddTaskController()
+const loadTasksController = new LoadTasksController()
+const updateTaskController = new UpdateTaskController()
+const removeTaskController = new RemoveTaskController()
 
 router.post('/task', addTaskController.handle)
+router.get('/task', loadTasksController.handle)
+router.put('/task/:id', updateTaskController.handle)
+router.delete('/task/:id', removeTaskController.handle)
 
 export { router }
---
+++ src/usecases/controllers.ts
@@ -0,0 +1,4 @@
+export * from './add-task/add-task-controller'
+export * from './update-task/update-task-controller'
+export * from './remove-task/remove-task-controller'
+export * from './load-tasks/load-tasks-controller'
